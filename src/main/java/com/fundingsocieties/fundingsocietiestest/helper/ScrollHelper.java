0f4738c0065277aad1cee082c84c1ffaae2dc9e6
LAP01422 <lap01422@lap01422s-macbook-pro.local>
2022-11-30 16:09:50 +0700

    update testcase

--- src/main/java/com/fundingsocieties/fundingsocietiestest/helper/ScrollHelper.java
+++ src/main/java/com/fundingsocieties/fundingsocietiestest/helper/ScrollHelper.java
@@ -1,2 +1,15 @@
-package com.fundingsocieties.fundingsocietiestest.helper;public class ScrollHelper {
+package com.fundingsocieties.fundingsocietiestest.helper;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class ScrollHelper {
+    private WebDriver driver;
+
+    public ScrollHelper(WebDriver driver) { this.driver = driver; }
+
+    public void scrollToMiddlePage (){
+        JavascriptExecutor scrollPage = ((JavascriptExecutor)driver);
+        scrollPage.executeScript("window.scrollBy(0,500)");
+    }
 }
--- src/main/java/com/fundingsocieties/fundingsocietiestest/pageobject/PageObject.java
+++ src/main/java/com/fundingsocieties/fundingsocietiestest/pageobject/PageObject.java
@@ -1,6 +1,7 @@
 package com.fundingsocieties.fundingsocietiestest.pageobject;
 
 import com.fundingsocieties.fundingsocietiestest.helper.ElementHelper;
+import com.fundingsocieties.fundingsocietiestest.helper.SwitchHelper;
 import com.fundingsocieties.fundingsocietiestest.helper.WaitHelper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -9,11 +10,14 @@ public abstract class PageObject {
     protected WebDriver driver;
     protected WaitHelper waitHelper;
     protected ElementHelper elementHelper;
+    protected SwitchHelper switchHelper;
 
     public PageObject(WebDriver driver) {
         this.driver = driver;
         waitHelper = new WaitHelper(driver);
         elementHelper = new ElementHelper(driver);
+        switchHelper = new SwitchHelper(driver);
         PageFactory.initElements(this.driver, this);
     }
+
 }
